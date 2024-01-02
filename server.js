ff55253ef4bba5d84a3c9c1fad6b188ce5ee7b31
mfnjairah <jairahtracks@gmail.com>
2024-01-02 16:12:31 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const domain = "jaisocket.netlify.app";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", domain],
+  },
+});
+
+const corsOptions = {
+  origin: ["http://localhost:3000", domain],
+  methods: ["GET", "POST"],
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("Worked");
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_message", data);
+  });
+
+  socket.on("content_change", (data) => {
+    socket.broadcast.emit("received_change", data);
+  });
+
+  socket.on("change_content", (data) => {
+    socket.broadcast.emit("changed", data);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
